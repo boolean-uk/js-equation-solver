83d8c2e15ba81746275e12bb7baed88c8593e9bb
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-30 20:14:36 +0200

    Commenting

--- src/equationsolver.js
+++ src/equationsolver.js
@@ -15,10 +15,17 @@ class EquationSolver {
     this.GROUP_REGEX = /\([^()]+\)/g;
   }
 
-
+  /** 
+   * Calculates the given equation.
+   * In case there's a group of operations, it will be calculated first.
+   * The memory key is use to pass retained data from one calculation to the next.
+   * The Fn key is used to pass the function to be used for the next calculation. (e.g. +, -, *, /)
+   * @param {string} operation - The simplified equation.
+   * @param {object} memory - The memory object.
+   * @param {string} Fn - The function to be used for the next calculation.
+  */
   calculate(operation, memory, Fn = (n) => n) {
     operation.match(this.OPERATOR_REGEX).map((char) => {
-
       this.operations[char]
         ? (Fn = this.operations[char])
         : (memory = Fn(Number(char), memory))
@@ -26,6 +33,11 @@ class EquationSolver {
     return memory
   }
 
+  /**
+   * Reduces the given equation to a simples equations.
+   * Removing parenthesis and grouping operations using REGEX.
+   * 10+(2*3) => ["10+", "2*3"] => 10+6 => 16
+   */
   solve(equation) {
     while (equation.includes('(') || equation.includes(')'))
       equation
