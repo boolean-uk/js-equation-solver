bfaa7de881bc34d43c901a07b320e7ce7b106872
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-31 10:28:20 +0200

    Refactor for readability

--- src/equationsolver.js
+++ src/equationsolver.js
@@ -10,7 +10,7 @@ class EquationSolver {
    * instead of using params to store data. But I thought it would be nice to see that it is possible to
    * shorten the code but still make it readable.
    *
-   * This isn't the best solution but is the shortest one.
+   * This isn't the best solution but is the shortest one. (With linted code)
    */
 
   constructor() {
@@ -19,7 +19,7 @@ class EquationSolver {
       '*': (c, a) => a * c,
       '/': (c, a) => a / c,
       '-': (c, a) => a - c,
-      '^': (c, a) => Math.pow(a, c)
+      '^': (c, a) => a ** c
     }
     this.OPERATOR_REGEX = /((^-|(?<=[\W]))-?)?(\.?\d(e[+-])?)+|[-*^+/]/g
     this.GROUP_REGEX = /\([^()]+\)/g
@@ -29,17 +29,20 @@ class EquationSolver {
    * Calculates the given equation.
    * In case there's a group of operations, it will be calculated first.
    * The memory key is use to pass retained data from one calculation to the next.
-   * The Fn key is used to pass the function to be used for the next calculation. (e.g. +, -, *, /)
+   * The operationFn key is used to pass the function to be used for the next calculation. (e.g. +, -, *, /)
    * @param {string} operation - The simplified equation.
-   * @param {object} memory - The memory object.
-   * @param {string} Fn - The function to be used for the next calculation.
+   * @constant {object} memory - The memory object.
+   * @constant {string} operationFn - The function to be used for the next calculation.
    */
-  calculate(operation, Fn = (n) => n) {
-    operation.match(this.OPERATOR_REGEX).map((char) => {
-      (this.operations[char] && (Fn = this.operations[char])) ||
-        (operation = Fn(Number(char), operation || 0))
+  calculate(operation) {
+    let operationFn
+    let memory
+    operation.match(this.OPERATOR_REGEX).forEach((char) => {
+      if (this.operations[char]) operationFn = this.operations[char]
+      else if (operationFn) memory = operationFn(Number(char), memory || 0)
+      else memory = Number(char)
     })
-    return operation
+    return memory
   }
 
   /**
@@ -49,21 +52,13 @@ class EquationSolver {
    * 10+6 (Simplest form of the full operation) will be calculated on the return of the function.
    */
   solve(equation) {
-    while (equation.match(this.GROUP_REGEX)) {
+    while (equation.match(this.GROUP_REGEX))
       equation = equation.replace(this.GROUP_REGEX, (group) =>
         this.calculate(group)
       )
-    }
 
     return this.calculate(equation)
   }
 }
-
-console.log(new EquationSolver().solve('-539+(20-10)+20'))
-console.log(
-  new EquationSolver().solve(
-    '((((378-(209*-959))+204+(231--297)/((598/(-348*-744))-191+(-427/-918))/(222*(-530--977)+(710*-245+8))*(671-39*-678-(98/-718)--390/-45))*((((8-(326*264))*683+(-3*-117))/(-148-52-921+-984-(451/204)))+(-665+128/-165/268*227*-572+(-668-796--969)/-372+-829/937*287)))*((-348-(-310/189)/(569-(438/-849))-((903/(662+730))+(247/(-792+411))))-((-60/-521+767)+497*-572-66*(-597+695*-650)-((431*927)/613-347)))*-200+277*-27-(-597*(-155+942))*530--800+-189+-598+(359+-562)+((151/888+200)--461*609*56-((739+144+469)+((-565-239)--482+985))))'
-  )
-)
 
 module.exports = EquationSolver
